66c8f07be05b6f3cf207dc9f3d22a2e4fddc5c5f
Toon <45061689+Antjul@users.noreply.github.com>
2023-12-16 13:49:38 +0100

    Update reviewDB.js

--- API/controleur/reviewDB.js
+++ API/controleur/reviewDB.js
@@ -3,17 +3,79 @@ const commentModele = require('../modele/commentDB');
 const reviewModele = require('../modele/reviewDB');
 
 
-module.exports.getAllReviews = async (req, res) => {
-    if(req.session !== undefined){
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *     Review:
+ *      type: object
+ *      properties:
+ *          id:
+ *              type: integer
+ *              description: The auto-generated id of the review
+ *          title:
+ *              type: string
+ *              description: The title of the review
+ *          content:
+ *              type: string
+ *              description: The content of the review
+ *          rating:
+ *              type: integer
+ *              description: The rating of the review
+ *          user_id:
+ *              type: integer
+ *              description: The id of the user who posted the review
+ *          book_id:
+ *              type: integer
+ *              description: The id of the book reviewed
+ *      required:
+ *          - title
+ *          - content
+ *          - rating
+ *          - user_id
+ *          - book_id
+ *      example:
+ *          id: 1
+ *          title: "A great book"
+ *          content: "I really enjoyed this book"
+ *          rating: 5
+ *          user_id: 1
+ *          book_id: 1
+ */
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Reviews
+ *   description: The reviews managing API
+ * /reviews:
+ *   get:
+ *     summary: Returns the list of all the reviews
+ *     tags: [Reviews]
+ *     responses:
+ *       200:
+ *         description: The list of the reviews
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Review'
+ *       500:
+ *         description: Some server error
+ */
+
+
+module.exports.getAllReview = async (req, res) => {
+    if(req.session === undefined){
     const client = await pool.connect();
     try{
-        const {rows: reviews} = await reviewModele.getAllReviews(client);
+        const {rows: reviews} = await reviewModele.getAllReview(client);
         if(reviews.length > 0){
             // display all reviews
-            console.log("reviews: ", reviews);
             res.json(reviews);
         } else {
-            console.log("No review found");
             res.sendStatus(404);
         }    }
     catch (e) {
@@ -25,9 +87,37 @@ module.exports.getAllReviews = async (req, res) => {
 }
 }
 
+/**
+ * @swagger
+ * /reviews/{id}:
+ *   get:
+ *     summary: Get the review with the given id
+ *     tags: [Reviews]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The review id
+ *     responses:
+ *       200:
+ *         description: The review description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Review'
+ *       400:
+ *         description: The id of the review is not a number
+ *       404:
+ *         description: The review was not found
+ *       500:
+ *         description: Some server error
+ */
+
 
 module.exports.getReview = async (req, res) => {
-    if(req.session !== undefined){
+    if(req.session === undefined){
     const client = await pool.connect();
     const idTexte = req.params.id; //attention ! Il s'agit de texte !
     const id = parseInt(idTexte);
@@ -52,15 +142,36 @@ module.exports.getReview = async (req, res) => {
 }
 }
 
+/**
+ * @swagger
+ * /reviews:
+ *   post:
+ *     summary: Create a new review
+ *     tags: [Reviews]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Review'
+ *     responses:
+ *       201:
+ *         description: The review was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Review'
+ *       500:
+ *         description: Some server error
+*/
 
 module.exports.postReview = async (req, res) => {
-    if(req.session !== undefined){
-    const review = req.body;
+    if(req.session === undefined){
+    const body = req.body;
+    const {title, content, book_id, rating} = body;
     const client = await pool.connect();
-    const userId = parseInt(req.session.id);
-    console.log("userId: ", userId);
     try{
-        const {rows} = await reviewModele.postReview(client, review.title, review.content, review.rating, userId, review.book_id);
+        const {rows} = await reviewModele.postReview(client, title, content, rating, 2, book_id);
         res.sendStatus(201).send(rows[0].id);
     } catch (error){
         console.error(error);
@@ -71,16 +182,46 @@ module.exports.postReview = async (req, res) => {
 }
 }
 
+/**
+ * @swagger
+ * /reviews/{id}:
+ *   patch:
+ *     summary: Update the review with the given id
+ *     tags: [Reviews]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The review id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Review'
+ *     responses:
+ *       204:
+ *         description: The review was successfully updated
+ *       400:
+ *         description: The id of the review is not a number
+ *       500:
+ *         description: Some server error
+*/
+
 module.exports.updateReview = async (req, res) => {
-    if(req.session !== undefined){
-    const review = req.body;
+    if(req.session === undefined){
+    const {title, content, rating} = req.body;
     const idReview = parseInt(req.params.id);
-    console.log("idReview: ", idReview);
-    console.log("review: ", review);
     const client = await pool.connect();
     try{
-        await reviewModele.updateReview(client, idReview, review.title, review.content, review.rating);
-        res.sendStatus(204);
+        if(isNaN(idReview)){
+            res.sendStatus(400);
+        }else{
+            await reviewModele.updateReview(client, idReview, title, content, rating);
+            res.sendStatus(204);
+        }
     } catch (error){
         console.error(error);
         res.sendStatus(500);
@@ -90,8 +231,31 @@ module.exports.updateReview = async (req, res) => {
 }
 }
 
+/**
+ * @swagger
+ * /reviews/{id}:
+ *   delete:
+ *     summary: Delete the review with the given id
+ *     tags: [Reviews]
+ *     parameters:
+ *          - in: path
+ *              name: id
+ *              schema:
+ *                  type: integer
+ *          required: true
+ *          description: The review id
+ *     responses:
+ *       204:
+ *         description: The review was deleted
+ *       400:
+ *         description: The id of the review is not a number
+ *       500:
+ *         description: Some server error
+ */
+
+
 module.exports.deleteReview = async (req, res) => {
-    if(req.session !== undefined){
+    if(req.session === undefined){
     const idTexte = req.params.id;
     const id = parseInt(idTexte);
     const client = await pool.connect();
@@ -99,14 +263,14 @@ module.exports.deleteReview = async (req, res) => {
     try {
         if (isNaN(id)) {
             res.sendStatus(400);
-            return;
+        } else {
+            await client.query("BEGIN");
+            await commentModele.deleteCommentsFromReview(client, id);
+            await reviewModele.deleteReview(client, id);
+            await client.query("COMMIT");
+            res.sendStatus(204);
         }
-        await client.query("BEGIN");
-        await commentModele.deleteCommentsFromReview(client, id);
-        await reviewModele.deleteReview(client, id);
-        await client.query("COMMIT");
-
-        res.sendStatus(204);
+        
     } catch (error) {
         await client.query("ROLLBACK");
         console.error(error);
@@ -116,5 +280,3 @@ module.exports.deleteReview = async (req, res) => {
     }
 }
 };
-
-
