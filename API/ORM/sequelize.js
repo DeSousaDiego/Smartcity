67ded9f0a1a794a3cff7ab02ee5bc918a91893af
Toon <45061689+Antjul@users.noreply.github.com>
2023-12-20 18:31:01 +0100

    Delete API/ORM directory

--- API/ORM/sequelize.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('exercices', 'john', 'password', {
-    host: 'localhost',
-    port: '5432',
-    dialect: 'postgres',
-    omitNull: true
-});
-
-module.exports = sequelize;
