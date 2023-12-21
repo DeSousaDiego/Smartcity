61b0f62f3956291589159976831ec186cb1843ea
de Sousa Barbosa Diego <81906437+DeSousaDiego@users.noreply.github.com>
2023-12-21 21:25:05 +0100

    Update userDB.js

--- API/controller/userDB.js
+++ API/controller/userDB.js
@@ -9,6 +9,7 @@ const {userSchema, userUpdateScheme} = require('../schema/ValidationSchemas');
 const {saveImage} = require('../model/imageManager');
 const uuid = require('uuid');
 const fs = require('fs');
+const { profile } = require("console");
 const destFolderImages = "./upload/avatars";
 
 module.exports.login = async (req, res) => {
@@ -81,7 +82,9 @@ module.exports.updateUser = async (req, res) => {
         const image = req.files?.image || null;
         let imageName = null;
         const {rows: users} = await UserModel.getUserById(client, toUpdate.id);
+        console.log("users", users);
         const user = users[0];
+        console.log("user", user);
         let doUpdate = false;
         let imageError = false
         newData.id = toUpdate.id;
@@ -89,103 +92,112 @@ module.exports.updateUser = async (req, res) => {
         if(userObj.id !== toUpdate.id && userObj.authLevel !== "admin"){
             res.sendStatus(401);
         }
-        // if the user is a simple user, he can't update the role
-        if(userObj.authLevel !== "admin"){
-            toUpdate.role = user.role;
-        }
-
-        if (image) {
-            // Vérifier si le fichier est une image valide
-            const  fileType = image[0].mimetype;
-            const validImageTypes = ['image/gif', 'image/jpeg', 'image/jpg', 'image/png'];
-            imageError = !validImageTypes.includes(fileType);
-            imageName = uuid.v4();
-        }
-        if(!imageError){
-            if(
-                toUpdate.username !== undefined ||
-                toUpdate.email_address !== undefined ||
-                toUpdate.password !== undefined ||
-                toUpdate.password2 !== undefined ||
-                toUpdate.role !== undefined ||
-                toUpdate.country !== undefined ||
-                toUpdate.phone_number !== undefined ||
-                toUpdate.news_letter !== undefined
-            ){
-                doUpdate = true;
+        else{
+            // if the user is a simple user, he can't update the role
+            if(userObj.authLevel !== "admin"){
+                toUpdate.role = user.role;
             }
-    
-            if(doUpdate){
-                newData.username = toUpdate.username;
-                newData.email_address = toUpdate.email_address;
-                newData.password = toUpdate.password;
-                newData.password2 = toUpdate.password2;
-                newData.role = toUpdate.role;
-                newData.country = toUpdate.country;
-                newData.phone_number = toUpdate.phone_number;
-                newData.news_letter = toUpdate.news_letter;
-                newData.image = imageName;                try{
-                        const results = userUpdateScheme.safeParse({
-                            username: newData.username,
-                            email_address: newData.email_address,
-                            password: newData.password,
-                            password2: newData.password2,
-                            role: newData.role,
-                            country: newData.country,
-                            phone_number: newData.phone_number,
-                            news_letter: (newData.news_letter === "true"),
-                            image: imageName
-                        });
 
-                        if (image) {
-                            await saveImage(image[0].buffer, imageName, destFolderImages);
-                        }
-            
-                        if (!results.success) {
-                            res.status(400).json("Erreur lors de la mise à jour des données");
-                        } else {
-                            const newUser = results.data;
-            
-                            await UserModel.updateUser(
-                                client,
-                                toUpdate.id,
-                                newUser.username,
-                                newUser.email_address,
-                                newUser.password,
-                                newUser.role,
-                                newUser.country,
-                                newUser.phone_number,
-                                newUser.news_letter,
-                                imageName,
-                            );
-                            let fileNameToDelete = null;
-                            let filePath = null;
-                            const {rows} = await UserModel.getUserById(client, toUpdate.id);
-                            if(rows[0].profile_picture_path){
-                                fileNameToDelete = rows[0].profile_picture_path
-                                filePath = `${destFolderImages}/${fileNameToDelete}.jpeg`;
-                                // Vérifier si le fichier existe avant de le supprimer
-                                if (fs.existsSync(filePath)) {
-                                    // Supprimer le fichier
-                                    fs.unlinkSync(filePath);
-                                }
+            if (image) {
+                // Vérifier si le fichier est une image valide
+                const  fileType = image[0].mimetype;
+                const validImageTypes = ['image/gif', 'image/jpeg', 'image/jpg', 'image/png'];
+                imageError = !validImageTypes.includes(fileType);
+                imageName = uuid.v4();
+            }
+            if(!imageError){
+                if(
+                    toUpdate.username !== undefined ||
+                    toUpdate.email_address !== undefined ||
+                    toUpdate.password !== undefined ||
+                    toUpdate.password2 !== undefined ||
+                    toUpdate.role !== undefined ||
+                    toUpdate.country !== undefined ||
+                    toUpdate.phone_number !== undefined ||
+                    toUpdate.news_letter !== undefined
+                ){
+                    doUpdate = true;
+                }
+
+                if(doUpdate){
+                    newData.username = toUpdate.username;
+                    newData.email_address = toUpdate.email_address;
+                    newData.password = toUpdate.password;
+                    newData.password2 = toUpdate.password2;
+                    newData.role = toUpdate.role;
+                    newData.country = toUpdate.country;
+                    newData.phone_number = toUpdate.phone_number;
+                    newData.news_letter = toUpdate.news_letter;
+                    newData.image = imageName;                
+                    try{
+                            const results = userUpdateScheme.safeParse({
+                                username: newData.username,
+                                email_address: newData.email_address,
+                                password: newData.password,
+                                password2: newData.password2,
+                                role: newData.role,
+                                country: newData.country,
+                                phone_number: newData.phone_number,
+                                news_letter: (newData.news_letter === "true"),
+                                image: imageName
+                            });
+
+                            if (image) {
+                                await saveImage(image[0].buffer, imageName, destFolderImages);
                             }
-                        res.sendStatus(204);
+                
+                            if (!results.success) {
+                                res.status(400).json("Erreur lors de la mise à jour des données");
+                            } else {
+                                const newUser = results.data;
+                
+                                await UserModel.updateUser(
+                                    client,
+                                    toUpdate.id,
+                                    newUser.username,
+                                    newUser.email_address,
+                                    newUser.password,
+                                    newUser.role,
+                                    newUser.country,
+                                    newUser.phone_number,
+                                    newUser.news_letter,
+                                    imageName,
+                                );
+                                let fileNameToDelete = null;
+                                let filePath = null;
+                                console.log("toUpdate", toUpdate);
+                                console.log("users", users);
+                                
+
+                                if(user.profile_picture_path){
+                                    fileNameToDelete = user.profile_picture_path
+                                    filePath = `${destFolderImages}/${fileNameToDelete}.jpeg`;
+                                    // Vérifier si le fichier existe avant de le supprimer
+                                    if (fs.existsSync(filePath)) {
+                                        // Supprimer le fichier
+                                        fs.unlinkSync(filePath);
+                                    }
+                                }
+                            res.sendStatus(204);
+                            }
                         }
-                    }
-                    catch (error) {
-                        console.error(error);
-                        res.sendStatus(500);
-                    } finally {
-                        client.release();
-                    }
+                        catch (error) {
+                            console.error(error);
+                            res.sendStatus(500);
+                        } finally {
+                            client.release();
+                        }
+                    
+                
+                }
+                else{
+                res.sendStatus(400);
+                }
+            } else{
+                res.status(400).json("Le fichier n'est pas une image valide.");
             }
-            else{
-             res.sendStatus(400);
-            }
-        } else{
-            res.status(400).json("Le fichier n'est pas une image valide.");
         }
+        
 
     } else {
         res.sendStatus(401);
@@ -325,19 +337,20 @@ module.exports.deleteUser = async (req, res) => {
         if(userObj.id !== id && userObj.authLevel !== "admin"){
             res.sendStatus(401);
         }
-    
-        try{
-            if(isNaN(id)){
-                res.status(400).json("L'id doit être un nombre");
-            } else {
-                await UserModel.deleteUser(client, id);
-                res.sendStatus(204);
+        else{
+            try{
+                if(isNaN(id)){
+                    res.status(400).json("L'id doit être un nombre");
+                } else {
+                    await UserModel.deleteUser(client, id);
+                    res.sendStatus(204);
+                }
+            } catch (error){
+                console.error(error);
+                res.sendStatus(500);
+            } finally {
+                client.release();
             }
-        } catch (error){
-            console.error(error);
-            res.sendStatus(500);
-        } finally {
-            client.release();
         }
     }
 }
@@ -359,4 +372,3 @@ module.exports.getAllUsers = async (req, res) => {
         client.release();
     }
 }
-
