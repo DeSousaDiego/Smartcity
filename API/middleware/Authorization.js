5f83b8778691dfd8c7d9ff6aeb1fd492a3d6cd26
Toon <45061689+Antjul@users.noreply.github.com>
2024-01-04 15:22:42 +0100

    Delete API directory

--- API/middleware/Authorization.js
+++
@@ -1,9 +0,0 @@
-module.exports.mustBeAdmin = (req, res, next) => {
-    console.log("req.session: ", req.session);
-    console.log("req.session.authLevel: ", req.session.authLevel);
-    if(req.session && req.session.authLevel === 'admin'){
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-}
--- API/route/role.js
+++
@@ -1,8 +0,0 @@
-const RoleControlleur = require('../controleur/roleDB');
-const JWTMiddleWare = require("../middleware/IdentificationJWT");
-const Router = require("express-promise-router");
-const router = new Router;
-
-router.get('/', JWTMiddleWare.identification, RoleControlleur.getRoles);
-
-module.exports = router;
