b3782774f035fbebaf463f9cd3b5253f2db3c94c
Toon <45061689+Antjul@users.noreply.github.com>
2024-01-15 21:03:30 +0100

    Delete API directory

--- API/route/actor.js
+++
@@ -1,8 +0,0 @@
-const ActorControlleur = require('../controleur/actorDB');
-const JWTMiddleWare = require("../middleware/IdentificationJWT");
-const Router = require("express-promise-router");
-const router = new Router;
-
-router.get('/', JWTMiddleWare.identification,ActorControlleur.getAllActors);
-
-module.exports = router;
